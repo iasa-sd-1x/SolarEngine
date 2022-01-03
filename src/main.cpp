b3ddb6c0fc2035a3a167915056cb3281dffe1f6c
KatanaMajesty <sasha.panasenk@gmail.com>
2022-01-03 16:48:22 +0200

    Переписал шаблонку

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <config.h>
+
 #include <iostream>
+
+#include <config.h>
+#include <solstd.h>
+
+
+void glew_log(std::string message)
+{
+    std::cout << "GLEW Log: " << message << std::endl;
+}
 
 void glfw_log(std::string message)
 {
@@ -12,9 +22,9 @@ int main()
     glfw_log("Started initializing GLFW");
     if (!glfwInit())
     { 
- 	glfw_log("Failed to initialize GLFW. Stopping..."); 
-	exit(EXIT_FAILURE);
-    }	
+        glfw_log("Failed to initialize GLFW. Stopping..."); 
+        exit(EXIT_FAILURE);
+    }
     glfw_log("GLFW was successfully initialized");
     GLFWwindow* window = glfwCreateWindow(M_WIDTH, M_HEIGHT, "Hello, World!", NULL, NULL);
     glfw_log("Started creating Main Window");
@@ -24,7 +34,18 @@ int main()
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
-    glfw_log("Main Window was successfully created");
+
+    // Подключение OpenGL OpenES, инициализация GLEW
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK)
+    {
+        glew_log("GLEW Init error");
+        exit(EXIT_FAILURE);
+    }
+    else glew_log("GLEW was successfully initialized");
+    glew_log(fstring("GLEW version: %s", glGetString(GL_VERSION)));
+
+    glfw_log(fstring("Main Window was successfully created: \nWindow Width -> %d\nWindow Height -> %d", M_WIDTH, M_HEIGHT));
     glfwSwapInterval(1);
     while (!glfwWindowShouldClose(window))
     {
